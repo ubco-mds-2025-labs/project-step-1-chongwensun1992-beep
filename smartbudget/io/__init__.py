1dec9ae7f9d7264bdd4e2ae424c2ca3b80105dc7
ChoWingDev <gladyschan0803@gmail.com>
2025-11-17 14:12:12 -0800

    Add files via upload
    updated io files

---
+++ smartbudget/io/__init__.py
@@ -0,0 +1,30 @@
+"""
+I/O subpackage for SmartBudget.
+
+Provides:
+- JSON input/output utilities
+- File system utilities
+"""
+
+from .json_io import (
+    save_to_json,
+    load_from_json
+)
+
+from .file_utils import (
+    file_exists,
+    delete_file,
+    list_files
+)
+
+__all__ = [
+    # JSON I/O
+    "save_to_json",
+    "load_from_json",
+    "json_pretty_print",
+
+    # File utilities
+    "file_exists",
+    "delete_file",
+    "list_files",
+]
