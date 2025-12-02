7a9ac662a5c876e21aae767ab9abe563f709dcab
chongwen sun <chongwen.sun1992@gmail.com>
2025-12-02 14:24:23 -0800

    updates core

---
+++ smartbudget/core/__init__.py
@@ -0,0 +1,40 @@
+"""
+Core package for SmartBudget.
+Exports menu functions and controller modules.
+"""
+
+from .controller_menu import print_menu, run
+from .controller_records import (
+    add_income,
+    add_expense,
+    show_summary,
+    show_income_details,
+    show_expense_details,
+)
+from .controller_system import (
+    save_data,
+    load_data,
+    show_files,
+    clear_data,
+    delete_backup_file,
+)
+
+__all__ = [
+    # menu
+    "print_menu",
+    "run",
+
+    # record controllers
+    "add_income",
+    "add_expense",
+    "show_summary",
+    "show_income_details",
+    "show_expense_details",
+
+    # system controllers
+    "save_data",
+    "clear_data",
+    "load_data",
+    "show_files",
+    "delete_backup_file",
+]
